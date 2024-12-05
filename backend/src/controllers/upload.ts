c9b74debf9ee3cc181b6b8fd858beb53041c8390
osipqa <sery.osipow@yandex.ru>
2024-12-05 23:52:32 +0300

    Fix: Generate unique file names for uploads

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,28 +1,28 @@
 import { NextFunction, Request, Response } from 'express'
-import { constants } from 'http2'
 import BadRequestError from '../errors/bad-request-error'
 import { v4 as uuidv4 } from 'uuid'
+import path from 'path';
 
-export const uploadFile = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
+export const uploadFile = async (req: Request, res: Response, next: NextFunction) => {
     if (!req.file) {
-        return next(new BadRequestError('Файл не загружен'))
+        return next(new BadRequestError('Файл не загружен'));
     }
+
     try {
-        const uniqueFilename = `${uuidv4()}-${req.file.originalname}`
+        const fileExtension = path.extname(req.file.originalname);
+        const uniqueFileName = `${uuidv4()}${fileExtension}`;
+
         const fileName = process.env.UPLOAD_PATH
-            ? `/${process.env.UPLOAD_PATH}/${uniqueFilename}`
-            : `/${uniqueFilename}`
-        return res.status(constants.HTTP_STATUS_CREATED).send({
+            ? `/${process.env.UPLOAD_PATH}/${uniqueFileName}`
+            : `/${uniqueFileName}`;
+
+        return res.status(201).send({
             fileName,
-            originalName: req.file?.originalname,
-        })
+            originalName: req.file.originalname,
+        });
     } catch (error) {
-        return next(error)
+        return next(error);
     }
-}
+};
 
 export default {}
